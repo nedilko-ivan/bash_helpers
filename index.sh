a75f099ac2586aac44122be04c017a6aa9a40de9
Yuri Varshavsky <yuriv@fanhubmedia.com>
2022-01-12 09:04:36 +0200

    Add comments

--- index.sh
+++ index.sh
@@ -1,8 +1,8 @@
-# Enable SSH agent
+# Enable SSH agent (Useful when you use multiple git account)
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/some_key_file
 
-#How to sim link example
+#How to sim link example (Useful when you need reference to some files in other folder. No need to copy paste)
 # cd to the folder where we need to create sim link
 ln -s ../../../some_folder folder_to_sim_link
 # first arg is from WHERE we need to create sim link and second argument target
